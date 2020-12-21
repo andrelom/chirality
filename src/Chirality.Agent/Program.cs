98fab30860fabd815de514c756b614d5b128cd19
André Moraes <andrelom@gmail.com>
2020-12-21 02:04:41 -0300

    Update Program.cs

--- src/Chirality.Agent/Program.cs
+++ src/Chirality.Agent/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Chirality.Core;
+using Chirality.Core.Utilities;
+using Chirality.P2P;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Chirality.Agent
 {
     internal static class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
-            Console.WriteLine("Chirality!");
+            var token = ProcessUtility.CreateCancellationToken();
+            var host = new HostBuilder()
+                .ConfigureServices(ConfigureServices)
+                .Build();
+
+            try
+            {
+                await host.RunAsync(token);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(Messages.ShutdownDueUnexpectedError);
+            }
+            finally
+            {
+                await Task.Delay(250, token);
+            }
         }
+
+        #region Private Methods
+
+        private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            // Hosted Services
+            services.AddHostedService<Server>();
+        }
+
+        #endregion
     }
 }
