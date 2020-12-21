7c5e7936375bb2ca325b511e41775e3dc2610683
André Moraes <andrelom@gmail.com>
2020-12-21 01:20:05 -0300

    Add Chirality.Agent.csproj

---
+++ src/Chirality.Agent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chirality.Agent
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Chirality!");
+        }
+    }
+}
