4928b1aa6e03ecac39f99679c6d4eb642a1de394
André Moraes <andrelom@gmail.com>
2020-12-21 02:19:55 -0300

    Update Constants.cs

--- src/Chirality.Agent/Constants.cs
+++ src/Chirality.Agent/Constants.cs
@@ -3,5 +3,7 @@ namespace Chirality.Agent
     public static class Constants
     {
         public static string SettingsFileName = "settings.json";
+
+        public static string LogFileName = "{Date}.log";
     }
 }
